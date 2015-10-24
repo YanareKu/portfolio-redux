27f53ecc13a0338abdce6c7e789b1df798885583
Yanare Ku <lurial@gmail.com>
2015-10-23 19:12:26 -0700

    Sorted out templates. Now to swap out placeholders and figure out how to launch it on bluehost.

--- portfolio.py
+++ portfolio.py
@@ -1,5 +1,4 @@
-from flask import Flask, request, render_template,  redirect, url_for,
-import model
+from flask import Flask, request, render_template
 import jinja2
 import os
 
@@ -11,6 +10,16 @@ app.jinja_env.undefined = jinja2.StrictUndefined
 def index():
     """Website opens with Illustration Gallery"""
     return render_template("illust.html")
+
+@app.route("/vector")
+def vector():
+    """Vector Gallery"""
+    return render_template("vector.html")
+
+@app.route("/ui")
+def ui():
+    """UI Gallery"""
+    return render_template("ui.html")
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
